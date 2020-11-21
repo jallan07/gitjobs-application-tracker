36f86fa05403688b64acb764e8ecfaa542a3d8e8
Josh Allan <allan.josh07@gmail.com>
2020-11-21 09:48:04 -0500

    Added the routes folder with basic routes, the linkedin keys file (in config folder) to store app id and secret, basic testing views with the EJS engine, and the code within app.js for running the server. Testing was a success, and linkedin oauth works.

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const passport = require('passport')
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy
+const routes = require('./routes/routes.js')
+const linkedinKeys = require('./config/linkedin-keys')
+
+app.set('view engine', 'ejs')
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET'
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user)
+})
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj)
+})
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: linkedinKeys.linkedinAuth.clientID,
+      clientSecret: linkedinKeys.linkedinAuth.clientSecret,
+      callbackURL: linkedinKeys.linkedinAuth.callbackURL,
+      scope: ['r_emailaddress', 'r_liteprofile']
+    },
+    function (token, tokenSecret, profile, done) {
+      return done(null, profile)
+    }
+  )
+)
+
+app.use('/', routes)
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log('App listening on port http://localhost:' + port)
+})
---
+++ config/linkedin-keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  linkedinAuth: {
+    clientID: '77qhe00w5z0yz9',
+    clientSecret: 'ZO2O6KbUg8K4ASxt',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  }
+}
