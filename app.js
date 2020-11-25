7c30a074082cdb5daf72f6264e7a6a9cd5bcdcaa
Josh Allan <63212805+jallan07@users.noreply.github.com>
2020-11-24 21:10:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const db = require('./models');
 
 // Body Parser
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
