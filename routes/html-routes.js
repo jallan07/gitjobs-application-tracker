8773eb56897e87634c2df120c019e484ce88d0c8
Baxter Cox <baxtercoxrtr@gmail.com>
2020-11-30 18:44:36 -0500

    initial handlebars commit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,9 @@
-
 //! This file offers a set of routes for sending users to the various html pages
 
 //* ===================================================
 //* Dependencies
 //* ===================================================
-const passport = require('passport');
+const passport = require("passport");
 
 //* ===================================================
 //* Routes
@@ -17,30 +16,30 @@ module.exports = (app) => {
   // Validate user is logged in
   const isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) return next();
-    res.redirect('/');
+    res.redirect("/");
   };
 
   // Get linkedin authentication
   app.get(
-    '/auth/linkedin',
-    passport.authenticate('linkedin', function (req, res) {
+    "/auth/linkedin",
+    passport.authenticate("linkedin", function (req, res) {
       console.log(res);
     })
   );
 
   // Get linkedin authentication callback
   app.get(
-    '/auth/linkedin/callback',
-    passport.authenticate('linkedin', {
-      successRedirect: '/profile',
-      failureRedirect: '/login'
+    "/auth/linkedin/callback",
+    passport.authenticate("linkedin", {
+      successRedirect: "/profile",
+      failureRedirect: "/login",
     })
   );
 
   // logout the user and send them back to the main index page
-  app.get('/logout', function (req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
   });
 
   //* ===================================================
@@ -48,20 +47,26 @@ module.exports = (app) => {
   //* ===================================================
   // index route loads main page
   // TODO Currently index.handlebars (CV page) - do we want this to be the job board?
-  app.get('/', (req, res) => {
-    res.render('index');
+  app.get("/", (req, res) => {
+    res.render("index");
   });
 
+  app.get("/login", (req, res) => {
+    res.render("pages/login", {
+      style: "login.css",
+    });
+  });
   // get the profile for logged in users
   //TODO Currently index.handlebars serves as profile page - swap this for job board?
-  app.get('/profile', isLoggedIn, (req, res) => {
-    res.render('pages/profile.ejs', {
-      user: req.user
+  app.get("/profile", (req, res) => {
+    res.render("pages/profile", {
+      user: req.user,
+      style: "profile.css",
     });
   });
 
   // get the rolodex for the logged in user
-  app.get('/rolodex', function (req, res) {
-    res.render('rolodex');
+  app.get("/rolodex", function (req, res) {
+    res.render("rolodex");
   });
 };
