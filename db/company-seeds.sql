735f7c6d8f957c61435e1319b400840a6cf24ada
MDC <markdcross@gmail.com>
2020-12-04 13:41:12 -0500

    Commenting out db to remove dupe table creation

--- db/company-seeds.sql
+++ db/company-seeds.sql
@@ -1,35 +1,35 @@
--- TODO Create a list of seed companies for the database to use as dummy data
+-- -- TODO Create a list of seed companies for the database to use as dummy data
 
-INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("Tesla", 50000, 20000000000, "tesla.com", "Energy", "Automotive", "Palo Alto", "CA", "Tesla, Inc. is an American electric vehicle and clean energy company based in Palo Alto, California. Tesla's current products include electric cars, battery energy storage from home to grid scale, solar panels and solar roof tiles, and related products and services.", "2020-12-01 11:05:27", "2020-12-01 11:05:27");
-
-
-INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("Capital One", 50000, 28000000000, "capitalone.com", "Finance", "Banking", "McLean", "VA", "Capital One Financial Corporation is an American bank holding company specializing in credit cards, auto loans, banking, and savings accounts, headquartered in McLean, Virginia with operations primarily in the United States.", "2020-12-03 09:18:24", "2020-12-03 09:18:24");
+-- INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("Tesla", 50000, 20000000000, "tesla.com", "Energy", "Automotive", "Palo Alto", "CA", "Tesla, Inc. is an American electric vehicle and clean energy company based in Palo Alto, California. Tesla's current products include electric cars, battery energy storage from home to grid scale, solar panels and solar roof tiles, and related products and services.", "2020-12-01 11:05:27", "2020-12-01 11:05:27");
 
 
-INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("Paymerang", 75, 5000000, "paymerang.com", "Technology", "Finance", "Richmond", "VA", "Paymerang provides electronic payment solutions for clients around the country in education, healthcare, media, manufacturing, services and beyond. ", "2020-12-03 09:23:46", "2020-12-03 09:23:46");
+-- INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("Capital One", 50000, 28000000000, "capitalone.com", "Finance", "Banking", "McLean", "VA", "Capital One Financial Corporation is an American bank holding company specializing in credit cards, auto loans, banking, and savings accounts, headquartered in McLean, Virginia with operations primarily in the United States.", "2020-12-03 09:18:24", "2020-12-03 09:18:24");
 
 
-INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("Amazon", 800000, 280000000000, "amazon.com", "Technology", "Retail", "Seattle", "WA", "Amazon.com, Inc., is an American multinational technology company based in Seattle, Washington, which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.", "2020-12-03 09:26:34", "2020-12-03 09:26:34");
+-- INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("Paymerang", 75, 5000000, "paymerang.com", "Technology", "Finance", "Richmond", "VA", "Paymerang provides electronic payment solutions for clients around the country in education, healthcare, media, manufacturing, services and beyond. ", "2020-12-03 09:23:46", "2020-12-03 09:23:46");
 
 
-INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("Google", 98000, 160000000000, "google.com", "Technology", "Advertising", "Mountain View", "CA", "Google, LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware.", "2020-12-03 09:28:17", "2020-12-03 09:28:17");
+-- INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("Amazon", 800000, 280000000000, "amazon.com", "Technology", "Retail", "Seattle", "WA", "Amazon.com, Inc., is an American multinational technology company based in Seattle, Washington, which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.", "2020-12-03 09:26:34", "2020-12-03 09:26:34");
 
- INSERT INTO Companies
- (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
-VALUES
- ("CarMax", 24000, 17000000000, "carmax.com", "Retail", "Automotive", "Richmond", "VA", "CarMax is America's largest used-car retailer and a Fortune 500 company. The corporate entity behind the formation of CarMax was Circuit City Stores, Inc. The first CarMax used-car store opened in September 1993, 1.7 miles from Circuit City's corporate offices in Richmond, Virginia.", "2020-12-03 09:30:54", "2020-12-03 09:30:54");
+
+-- INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("Google", 98000, 160000000000, "google.com", "Technology", "Advertising", "Mountain View", "CA", "Google, LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware.", "2020-12-03 09:28:17", "2020-12-03 09:28:17");
+
+--  INSERT INTO Companies
+--  (companyName, companyEmployees, companyRevenue, companyWebsite, companySector, companyIndustry, companyCity, companyState, companyDescription, createdAt, updatedAt) 
+-- VALUES
+--  ("CarMax", 24000, 17000000000, "carmax.com", "Retail", "Automotive", "Richmond", "VA", "CarMax is America's largest used-car retailer and a Fortune 500 company. The corporate entity behind the formation of CarMax was Circuit City Stores, Inc. The first CarMax used-car store opened in September 1993, 1.7 miles from Circuit City's corporate offices in Richmond, Virginia.", "2020-12-03 09:30:54", "2020-12-03 09:30:54");
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
--- Drops the gitjobs_db if it exists currently --
-DROP DATABASE IF EXISTS gitjobs_db;
--- Creates the "gitjobs_db" database --
-CREATE DATABASE gitjobs_db;
+-- -- Drops the gitjobs_db if it exists currently --
+-- DROP DATABASE IF EXISTS gitjobs_db;
+-- -- Creates the "gitjobs_db" database --
+-- CREATE DATABASE gitjobs_db;
 
 -- USE gitjobs_db;
 
